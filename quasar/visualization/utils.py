f9598e5ad4cc9b4132058c04c42778c2ac8a88fd
Christoph Alt <christoph.alt@dfki.de>
2018-05-18 21:21:56 +0200

    unit tests for visualization and datasets

--- quasar/visualization/utils.py
+++ quasar/visualization/utils.py
@@ -1,55 +1,62 @@
-import plotly.graph_objs as go
 
 from toolz import dicttoolz
 
 
-def hparams_to_parallel_coordinates(hparams, precision=3, remove_single_values=True):
+def trials_to_dimensions(trials, precision=6, remove_single_values=True):
     """
-    Converts given hyperparameter configurations into the Plotly parallel coordinates
-    format.
+    Converts hyperparameter configurations into the Plotly dimensions, e.g. used
+    to plot parallel coordinates.
 
 
     Args:
-        hparams (list): Hyperparameter configurations for a sequence of trials.
+        trials (list): Hyperparameter configurations for a sequence of trials.
         A trial is represented by a dict of hyperparameters and corresponding values.
         precision (int, optional): The precision for displaying values of type float.
         remove_single_values (bool, optional): If to remove hyperparameters with a single unique value.
+
+    Returns:
+        :class:`list` of :class:`dict`: A list of dictionaries describing each hyperparameter dimension.
     """
 
     #TODO: If floats are below 1., switch to exponential notation
     #TODO: If displaying floats as strings, sort them numerically
-    
-    def format_values(values):
-        if not values:
-            return values
-        
-        elem = values[0]
-        if isinstance(elem, float):
-            fmt = '{{:.{}f}}'.format(precision)
-            values = [float(fmt.format(value)) for value in values]
-        elif isinstance(elem, str):
-            values = list(range(len(set(values))))
-        return values
+    #TODO: Scale axis so ticks are not overlapping
     
     def to_dimension_dict(hparam):
         label, values = hparam
-        formated_values = format_values(values)
-        dct = dict(range=[min(formated_values), max(formated_values)],
+
+        elem = values[0]
+        if isinstance(elem, str):
+            unique = list(sorted(set(values)))
+            stoi = {v: i for i, v in enumerate(unique)}
+            tick_values = list(range(len(unique)))
+            tick_text = unique
+            values = [stoi[v] for v in values]
+        elif isinstance(elem, float):
+            fmt = '{{:.{}g}}'# if max(values) <= 1 else '{{:.{}f}}'
+            fmt = fmt.format(precision)
+            tick_values = list(sorted(set(values)))
+            tick_text = [fmt.format(v) for v in tick_values]
+        else:
+            tick_values = list(sorted(set(values)))
+            tick_text = [str(v) for v in tick_values]
+
+        dct = dict(range=[min(values), max(values)],
                    label=label,
-                   values=formated_values,
-                   tickvals=list(sorted(set(formated_values))))
+                   values=values,
+                   tickvals=tick_values,
+                   ticktext=tick_text)
         
-        if values and isinstance(values[0], str):
-            stoi = {v: i for i, v in enumerate(sorted(set(values)))}
-            dct['values'] = [stoi[value] for value in values]
-            dct['ticktext'] = [v for v, i in sorted(stoi.items(), key=lambda x: x[1])]
+        #if values and isinstance(values[0], str):
+        #    stoi = {v: i for i, v in enumerate(sorted(set(values)))}
+        #    dct['values'] = [stoi[value] for value in values]
+        #    dct['ticktext'] = [v for v, i in sorted(stoi.items(), key=lambda x: x[1])]
         return dct
 
-    hparams = dicttoolz.merge_with(list, hparams)
-    
+    hparams = dicttoolz.merge_with(list, trials)
     dimensions = [to_dimension_dict(hparam) for hparam in hparams.items()]
     
     if remove_single_values:
         dimensions = list(filter(lambda d: len(d['tickvals']) > 1, dimensions))
     
-    return go.Parcoords(line=dict(color = 'blue'), dimensions=dimensions)
+    return dimensions
--- quasar/visualization/visdom.py
+++ quasar/visualization/visdom.py
@@ -1,10 +1,13 @@
 import numpy as np
 
+import plotly.graph_objs as go
+
 
 def create_plot_window(vis, x_label, y_label, title):
     return vis.line(X=np.array([1]), Y=np.array([np.nan]),
                     opts=dict(xlabel=x_label, ylabel=y_label, title=title))
 
 
-def parallel_coordinates_window(vis, data, title):
-    vis._send({'data': [data], 'layout': dict(title=title)})
+def parallel_coordinates_window(vis, dimensions, title, line_color='blue'):
+    data = go.Parcoords(line=dict(color = line_color), dimensions=dimensions)
+    return vis._send({'data': [data], 'layout': dict(title=title)})
